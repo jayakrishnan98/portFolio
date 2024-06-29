97617e989cfc7b6e690d1e3c6f9ebcfc8782019e
jayakrishnan.vr <jayakrishnan.vr@knowledgelens.com>
2024-06-30 00:48:31 +0530

    form submission

--- js/form.js
+++ js/form.js
@@ -1,49 +1,29 @@
 $(function() {
+    var form = $('#ajax-contact');
+    var formMessages = $('#form-messages');
 
-	// Get the form.
-	var form = $('#ajax-contact');
+    $(form).submit(function(e) {
+        e.preventDefault(); // Prevent default form submission
+        
+        var formData = $(form).serialize();
 
-	// Get the messages div.
-	var formMessages = $('#form-messages');
-
-	// Set up an event listener for the contact form.
-	$(form).submit(function(e) {
-		// Stop the browser from submitting the form.
-		e.preventDefault();
-
-		// Serialize the form data.
-		var formData = $(form).serialize();
-
-		// Submit the form using AJAX.
-		$.ajax({
-			type: 'POST',
-			url: $(form).attr('action'),
-			data: formData
-		})
-		.done(function(response) {
-			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('bg-danger');
-			$(formMessages).addClass('bg-success');
-
-			// Set the message text.
-			$(formMessages).text('Your message successfully sent');
-
-			// Clear the form.
-			$('#name, #email, #message').val('');			
-		})
-		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('bg-success');
-			$(formMessages).addClass('bg-danger');
-
-			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
-		});
-
-	});
-
+        $.ajax({
+            type: 'POST',
+            url: $(form).attr('action'), // Use the form's action attribute
+            data: formData
+        })
+        .done(function(response) {
+            $(formMessages).removeClass('bg-danger').addClass('bg-success');
+            $(formMessages).text('Your message was successfully sent.');
+            $('#name, #email, #message').val('');
+        })
+        .fail(function(data) {
+            $(formMessages).removeClass('bg-success').addClass('bg-danger');
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            } else {
+                $(formMessages).text('Oops! An error occurred and your message could not be sent.');
+            }
+        });
+    });
 });
